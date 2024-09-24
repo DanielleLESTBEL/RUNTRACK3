68a223c3151ad23ec131a8146879c0a53bbb7a92
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-09-24 16:33:32 +0200

    Jour2_Job5

---
+++ Jour02/Job05/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./index.css?v=<?php echo time(); ?>">
+    <title>JS Jour 2 - Job 5</title>
+    
+    
+</head>
+<body>
+    <h1> Faite rouler la molette ! </h1>
+
+
+
+    <footer id="footer"><p>Je  change de couleur ;-))</p></footer>
+
+    <script src="script.js?v=<?php echo time(); ?>"></script>
+
+</body>
+</html>
